9e335a9cb2ec91ed7f2ee85d27bd2ce04af6d2b4
Vivek Patidar <vivekpatidar775@gmail.com>
2024-11-02 21:06:18 +0530

    Enhanced Header component responsiveness: adjusted logo and user icon sizes, optimized spacing, and improved button styling for a seamless layout across various screen sizes.

--- src/components/Header.js
+++ src/components/Header.js
@@ -41,18 +41,26 @@ const Header = () => {
 
   return (
     <header className="absolute w-full top-0 z-20">
-      <div className="relative flex justify-between items-center px-10">
+      <div className="relative flex justify-between items-center px-4 sm:px-8 md:px-10">
         <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-75 z-0"></div>
 
-        <div className="flex items-center justify-between w-full relative z-10 px-16 my-5">
-          <img className="w-60 md:w-60" src={logo} alt="StreamOracle Logo" />
+        <div className="flex items-center justify-between w-full relative z-10 px-4 sm:px-8 md:px-16 my-4 sm:my-5">
+          <img
+            className="w-36 sm:w-48 md:w-60"
+            src={logo}
+            alt="StreamOracle Logo"
+          />
 
-          {isLoggedIn && ( // Show only when user is logged in
-            <div className="flex items-center space-x-4">
-              <img className="w-10 h-10" src={userIcon} alt="user-icon" />
+          {isLoggedIn && (
+            <div className="flex items-center space-x-2 sm:space-x-4">
+              <img
+                className="w-8 sm:w-10 h-8 sm:h-10"
+                src={userIcon}
+                alt="user-icon"
+              />
               <button
                 onClick={handleSignOut}
-                className="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-red-400 shadow-md transition duration-300"
+                className="bg-red-600 text-white px-3 sm:px-4 py-1 sm:py-2 rounded-md hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-red-400 shadow-md transition duration-300"
               >
                 Sign Out
               </button>
