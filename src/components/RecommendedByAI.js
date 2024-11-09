adb2e8663678efbc92ed49309fadb45f765be90f
Vivek Patidar <vivekpatidar775@gmail.com>
2024-11-09 11:51:04 +0530

    Added background image to RecommendedByAI page and updated Header component with homeIcon.
    Integrated backgroundImg as the full-screen background for the RecommendedByAI page. Applied Tailwind CSS classes (absolute, inset-0, object-cover, and object-center) to maintain the image's aspect ratio and cover the entire viewport without stretching.
    Wrapped the AiSearchBar component in a relative z-10 div to display it above the background image, preserving visibility and layout.

--- src/components/RecommendedByAI.js
+++ src/components/RecommendedByAI.js
@@ -1,10 +1,18 @@
 import AiSearchBar from "./AiSearchBar"
+import backgroundImg from "../myAssets/background .jpg";
 
 const RecommendedByAI = () => {
   return (
-    <div>
+    <div className="relative w-full h-screen">
+    <img
+      className="absolute inset-0 w-full h-full object-cover object-center z-0"
+      src={backgroundImg}
+      alt="background-image"
+    />
+    <div className="relative z-10">
       <AiSearchBar />
     </div>
+  </div>
   )
 }
 
