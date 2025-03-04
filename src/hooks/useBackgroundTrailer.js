4049cc38d5307c2a0ebc82201942985fcaef91f0
Vivek Patidar <vivekpatidar775@gmail.com>
2025-03-04 10:44:08 +0530

    Refactor useBackgroundTrailer to use fetchFromTMDB utility function
    - Refactored the useBackgroundTrailer hook to use the centralized fetchFromTMDB utility function.
      - Replaced direct fetch calls with fetchFromTMDB for cleaner and more maintainable code.
      - Added error handling to log errors if the API call fails.
    - Updated the useEffect dependency array to include movieId and movieTrailer for better reactivity.
    - Removed the dependency on API_OPTIONS from constants.

--- src/hooks/useBackgroundTrailer.js
+++ src/hooks/useBackgroundTrailer.js
@@ -1,30 +1,32 @@
 import { useDispatch, useSelector } from "react-redux";
-import { API_OPTIONS } from "../utils/constant";
 import { addTrailer } from "../utils/moviesSlice";
 import { useEffect } from "react";
+import { fetchFromTMDB } from "../utils/api";
 
 const useBackgroundTrailer = (movieId) => {
   const dispatch = useDispatch();
   const movieTrailer = useSelector((store) => store.movies.movieTrailer);
 
   const getMoviesVideo = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/" +
-        movieId +
-        "/videos?language=en-US",
-      API_OPTIONS
-    );
-    const json = await data.json();
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-
-    dispatch(addTrailer(trailer));
+    try {
+      const json = await fetchFromTMDB(
+        `movie/${movieId}/videos?language=en-US`
+      );
+      const filterData = json.results.filter(
+        (video) => video.type === "Trailer"
+      );
+      const trailer = filterData.length ? filterData[0] : json.results[0];
+      dispatch(addTrailer(trailer));
+    } catch (error) {
+      console.error("Error fetching movie trailer:", error);
+    }
   };
+
   useEffect(() => {
     !movieTrailer && getMoviesVideo();
-  }, []);
+  }, [movieId, movieTrailer]);
+
+  return null;
 };
 
 export default useBackgroundTrailer;
