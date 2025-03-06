2a3a31edac95617148cf0c24d8f3c8c82deb875b
Vivek Patidar <vivekpatidar775@gmail.com>
2025-03-06 11:24:36 +0530

    Refactor useTopRatedMovies and useUpcomingMovies hooks
    - Refactored useTopRatedMovies and useUpcomingMovies hooks to use the centralized fetchFromTMDB utility function.
      - Replaced direct fetch calls with fetchFromTMDB for cleaner and more maintainable code.
      - Added error handling to log errors if the API call fails.
    - Updated the useEffect dependency arrays to include respective state and dispatch for better reactivity.
    - Removed the dependency on API_OPTIONS from constants.

--- src/hooks/useTopRatedMovies.js
+++ src/hooks/useTopRatedMovies.js
@@ -1,24 +1,24 @@
 import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constant";
 import { useDispatch, useSelector } from "react-redux";
 import { addTopRatedMovies } from "../utils/moviesSlice";
+import { fetchFromTMDB } from "../utils/api";
 
 const useTopRatedMovies = () => {
   const dispatch = useDispatch();
   const topRatedMovies = useSelector((store) => store.movies.topRatedMovies);
 
   const getTopRatedMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/top_rated",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    dispatch(addTopRatedMovies(json.results));
+    try {
+      const json = await fetchFromTMDB("movie/top_rated");
+      dispatch(addTopRatedMovies(json.results));
+    } catch (error) {
+      console.error("Error fetching top rated movies:", error);
+    }
   };
 
   useEffect(() => {
     !topRatedMovies && getTopRatedMovies();
-  }, []);
+  }, [topRatedMovies, dispatch]);
 };
 
 export default useTopRatedMovies;
--- src/hooks/useUpcomingMovies.js
+++ src/hooks/useUpcomingMovies.js
@@ -1,23 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
 import { addUpcomingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constant";
+import { fetchFromTMDB } from "../utils/api";
 
 const useUpcomingMovies = () => {
   const dispatch = useDispatch();
   const upcomingMovies = useSelector((store) => store.movies.upcomingMovies);
 
   const getUpcomingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    dispatch(addUpcomingMovies(json.results));
+    try {
+      const json = await fetchFromTMDB("movie/upcoming?language=en-US&page=1");
+      dispatch(addUpcomingMovies(json.results));
+    } catch (error) {
+      console.error("Error fetching upcoming movies:", error);
+    }
   };
+
   useEffect(() => {
     !upcomingMovies && getUpcomingMovies();
-  }, []);
+  }, [upcomingMovies, dispatch]);
 };
 
 export default useUpcomingMovies;
