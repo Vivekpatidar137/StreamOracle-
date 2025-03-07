cd0cfd27acf1dc186c23d976d2c55279b1452234
Vivek Patidar <vivekpatidar775@gmail.com>
2025-03-07 13:17:42 +0530

    Refactor useFetchDetails to use fetchFromTMDB utility function
    - Refactored the useFetchDetails hook to use the centralized fetchFromTMDB utility function.
      - Replaced direct fetch calls with fetchFromTMDB for cleaner and more maintainable code.

--- src/hooks/useFetchDetails.js
+++ src/hooks/useFetchDetails.js
@@ -1,16 +1,12 @@
 import { useEffect, useState } from "react";
-import { API_OPTIONS } from "../utils/constant";
+import { fetchFromTMDB } from "../utils/api";
 
 const useFetchDetails = (id) => {
   const [movieDetails, setMovieDetails] = useState(null);
 
   const fetchDetails = async (movieId) => {
     try {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
-        API_OPTIONS
-      );
-      const data = await response.json();
+      const data = await fetchFromTMDB(`movie/${movieId}?language=en-US`);
       setMovieDetails(data);
     } catch (error) {
       console.error("Error fetching movie details:", error);
